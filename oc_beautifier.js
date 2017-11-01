7297a6f5c58ba595181fa1dfac104408d6e9fd5a
TimG <timoguic@gmail.com>
2017-11-01 10:33:57 +0100

    First commit

---
+++ oc_beautifier.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @require http://code.jquery.com/jquery-3.2.1.min.js
+// @name         OC beautifier
+// @version      0.1
+// @description  Improves OC dashboard for mentors
+// @author       Tim G
+// @match        https://*openclassrooms.com/mentorship/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // improve user profile
+    // change height of mentor profile
+    $('.mentorProfile').css('min-height', '100px');
+    $('.mentorProfile__content section').not('.mentorProfile__username').toggle();
+    $('.mentorProfile__username').click(function() {
+        $('.mentorProfile__content section').not('.mentorProfile__username').toggle();
+    });
+    // hide the student list
+    $('table.crud-list').hide();
+    $('table.crud-list').siblings('h2').click(function() {
+        $('table.crud-list').toggle();
+    });
+})();
